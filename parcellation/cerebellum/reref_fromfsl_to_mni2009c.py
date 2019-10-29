ab30ca9a7d8bf3366849840881e730f49417f13f
wiheto <william.thompson@ki.se>
2019-10-29 15:08:03 -0700

    style fixes

--- parcellation/cerebellum/reref_fromfsl_to_mni2009c.py
+++ parcellation/cerebellum/reref_fromfsl_to_mni2009c.py
@@ -1,7 +1,5 @@
-import os
 from nipype.interfaces.ants import ApplyTransforms
-import templateflow.api as tf 
-import nibabel as nib
+import templateflow.api as tf
 import shutil
 
 relpath = './parcellation/cerebellum/'
@@ -9,18 +7,18 @@ relpath = './parcellation/cerebellum/'
 transform = tf.get(template='MNI152NLin2009cAsym', suffix='xfm', extension='h5')
 reference = tf.get(template='MNI152NLin2009cAsym', suffix='T1w', resolution=1, desc='brain', extension='nii.gz')
 
-#img path 
+#img path
 imgs = ['tpl-MNI152NLin6Asym_res-01_atlas-King2019Cerebellum_dseg.nii.gz']
 out_imgs = [relpath + 'tpl-MNI152NLin2009cAsym_res-01_atlas-King2019Cerebellum_dseg.nii.gz']
 
 for n, i in enumerate(imgs):
     at = ApplyTransforms()
-    # Input: image to change in FSL 
+    # Input: image to change in FSL
     at.inputs.input_image = relpath + i
     # Ref
     at.inputs.reference_image = str(reference)
-    # transform-file. 
-    at.inputs.transforms = str(transform) 
+    # transform-file.
+    at.inputs.transforms = str(transform)
     at.inputs.interpolation = 'MultiLabel'
     at.run()
 
