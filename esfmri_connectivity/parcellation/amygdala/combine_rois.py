d3891a722a91846f282d73106eb331a0add4f92b
wiheto <william.thompson@ki.se>
2019-11-01 13:40:24 -0700

    amygdala changes (l and r) and frakenstein parcellation

--- esfmri_connectivity/parcellation/amygdala/combine_rois.py
+++ esfmri_connectivity/parcellation/amygdala/combine_rois.py
@@ -22,7 +22,7 @@ ind = list(labelinfo[labelinfo['labels'].isin(target)].index)
 central_roi = np.squeeze(img.get_data()[:, :, :, ind])
 
 # Corticomedial:
-target = ['AMY_CMN', 'AMY_AAA']
+target = ['ThreAMY_CMN', 'AMY_AAA']
 ind = list(labelinfo[labelinfo['labels'].isin(target)].index)
 corticomedial_roi = np.sum(img.get_data()[:, :, :, ind], axis=-1)
 
@@ -34,22 +34,33 @@ basolateral_roi = np.sum(img.get_data()[:, :, :, ind], axis=-1)
 # Create new roi by enforcing the roi with the highest probability to be discrete
 new_rois = np.zeros(img.shape[:-1])
 new_rois[(central_roi > corticomedial_roi) & (
-    central_roi > basolateral_roi) & (central_roi > 0.5)] = 1
+    central_roi > basolateral_roi) & (central_roi > 0.25)] = 1
 new_rois[(corticomedial_roi > central_roi) & (corticomedial_roi >
-                                              basolateral_roi) & (corticomedial_roi > 0.5)] = 2
+                                              basolateral_roi) & (corticomedial_roi > 0.25)] = 2
 new_rois[(basolateral_roi > central_roi) & (basolateral_roi >
-                                            corticomedial_roi) & (basolateral_roi > 0.5)] = 3
+                                            corticomedial_roi) & (basolateral_roi > 0.25)] = 3
 
+# Now with the discrete images we need to split L vs R
+# Make everything in the R side + 10
+new_rois[int(img.affine[0, -1]):] += 10
+new_rois[new_rois == 10] = 0
+new_rois[new_rois == 11] = 4
+new_rois[new_rois == 12] = 5
+new_rois[new_rois == 13] = 6
+
+# Make nifti
 new_img = nib.Nifti1Image(new_rois, img.affine)
-
 nib.save(new_img, relpath +
          'tpl-MNI152NLin6Asym_res-01_atlas-3roiamygdala_dseg.nii.gz')
 
 # Make metadata for dseg:ed image
-names = ['amygdala_central', 'amygdala_corticomedial', 'amygdala_basolateral']
-abbr = ['AMY_CEN', 'AMY_CM', 'AMY_BL']
-metainfo = pd.DataFrame(data={'name': names, 'abbr': abbr}, index=[1, 2, 3])
+names = ['amygdala_central_left', 'amygdala_corticomedial_left', 'amygdala_basolateral_left',
+         'amygdala_central_right', 'amygdala_corticomedial_right', 'amygdala_basolateral_right']
+abbr = ['AMY_CEN_L', 'AMY_CM_L', 'AMY_BL_L',
+        'AMY_CEN_R', 'AMY_CM_R', 'AMY_BL_R']
+metainfo = pd.DataFrame(
+    data={'name': names, 'abbr': abbr}, index=[1, 2, 3, 4, 5, 6])
 metainfo.to_csv(
     relpath + 'tpl-MNI152NLin6Asym_res-01_atlas-3roiamygdala_dseg.tsv', sep='\t')
 metainfo.to_csv(
-    relpath + 'tpl-MNI152NLin2009cAsym-01_atlas-3roiamygdala_dseg.tsv', sep='\t')
+    relpath + 'tpl-MNI152NLin2009cAsym_res-01_atlas-3roiamygdala_dseg.tsv', sep='\t')
---
+++ esfmri_connectivity/parcellation/create_parcellation.py
@@ -0,0 +1,95 @@
+import templateflow.api as tf
+import nibabel as nib
+import pandas as pd
+import numpy as np
+
+# Specify save path
+rel_path = './esfmri_connectivity/parcellation/'
+
+# Download Scaeffer400 atlas
+data = {}
+data_path = tf.get('MNI152NLin2009cAsym', resolution=1,
+                   atlas='Schaefer2018', desc='400Parcels7Networks', extension='.nii.gz')
+img_cortex = nib.load(str(data_path))
+data['cortex'] = img_cortex.get_data().astype(float)
+
+info_path = tf.get('MNI152NLin2009cAsym', atlas='Schaefer2018', desc='400Parcels7Networks', extension='tsv')
+info_cortex = pd.read_csv(str(info_path), sep='\t', index_col=[0])
+
+# Load cerebellum
+
+img_cerebellum = nib.load(rel_path + 'cerebellum/tpl-MNI152NLin2009cAsym_res-01_atlas-King2019Cerebellum_dseg.nii.gz')
+data['cerebellum'] = img_cerebellum.get_data().astype(float)
+info_cerebellum = pd.read_csv(rel_path + 'cerebellum/tpl-MNI152NLin2009cAsym_res-01_atlas-King2019Cerebellum_dseg.tsv', sep='\t', index_col=[0])
+# Remove background/0 from this dataframae
+info_cerebellum.drop(0, inplace=True)
+
+# Load amygdala
+
+img_amygdala = nib.load(rel_path + 'amygdala/tpl-MNI152NLin2009cAsym_res-01_atlas-3roiamygdala_dseg.nii.gz')
+data['amygdala'] = img_amygdala.get_data().astype(float)
+info_amygdala = pd.read_csv(rel_path + 'amygdala/tpl-MNI152NLin2009cAsym_res-01_atlas-3roiamygdala_dseg.tsv', sep='\t', index_col=[0])
+
+# Load subcortical
+
+img_subcortical = nib.load(rel_path + 'subcortical/tpl-MNI152NLin2009cAsym_res-01_atlas-HOSPA_desc-th50_dseg.nii.gz')
+data['subcortical'] = img_subcortical.get_data().astype(float)
+info_subcortical = pd.read_csv(rel_path + 'subcortical/tpl-MNI152NLin6Asym_atlas-HOSPA_dseg.tsv', sep='\t', index_col=[0])
+
+
+# Check that there is no overlap in the different dictionaries
+def calc_number_of_voxels_per_parcel(img, info):
+    number_of_voxels = []
+    for n in info.index: 
+        number_of_voxels.append(np.sum(img==n))
+    return pd.DataFrame(number_of_voxels, index=info.index)
+
+n_cortex = calc_number_of_voxels_per_parcel(data['cortex'], info_cortex)
+n_amygdala = calc_number_of_voxels_per_parcel(data['amygdala'], info_amygdala)
+n_subcortical = calc_number_of_voxels_per_parcel(data['subcortical'], info_subcortical)
+n_cerebellum = calc_number_of_voxels_per_parcel(data['cerebellum'], info_cerebellum)
+
+data_mod = {}
+for i, d in enumerate(data.keys()): 
+    data_mod[d] = data[d].copy()
+    for ii, dd in enumerate(data.keys()):
+        if i != ii: 
+            x = np.where(data[d].flatten()>0)[0]
+            y = np.where(data[dd].flatten()>0)[0]
+            intersec = set(x).intersection(y)
+            tmp = data_mod[d].flatten()
+            tmp[list(intersec)] = 0 
+            data_mod[d] = tmp.reshape(data_mod[d].shape)
+            if len(intersec) > 0: 
+                print('intersection: ' + d + ' and ' + dd + ': ' + str(len(intersec)))
+
+m_cortex = calc_number_of_voxels_per_parcel(data_mod['cortex'], info_cortex)
+m_amygdala = calc_number_of_voxels_per_parcel(data_mod['amygdala'], info_amygdala)
+m_subcortical = calc_number_of_voxels_per_parcel(data_mod['subcortical'], info_subcortical)
+m_cerebellum = calc_number_of_voxels_per_parcel(data_mod['cerebellum'], info_cerebellum)
+
+data_mod['cerebellum'] += 400 
+data_mod['cerebellum'][data_mod['cerebellum'] == 400] = 0
+info_cerebellum.index += 400
+
+data_mod['subcortical'] += 500 
+data_mod['subcortical'][data_mod['subcortical'] == 500] = 0
+info_subcortical.index += 500
+
+data_mod['amygdala'] += 600 
+data_mod['amygdala'][data_mod['amygdala'] == 600] = 0 
+info_amygdala.index += 600
+
+
+
+data_frank = data_mod['cortex'].copy()
+data_frank += data_mod['amygdala']
+data_frank += data_mod['cerebellum']
+data_frank += data_mod['subcortical']
+
+img_frank = nib.Nifti1Image(data_frank, img_cortex.affine)
+savename = 'tpl-MNI152NLin2009cAsym_res-01_atlas-frankenstein_dseg'
+nib.save(img_frank, rel_path + savename + '.nii.gz')
+
+info_frank = pd.concat([info_cortex, info_cerebellum, info_subcortical, info_amygdala], sort=True)  
+info_frank.to_csv(rel_path + savename + '.tsv', sep='\t')
