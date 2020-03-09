024b5a5a1a182231b4bbce617c7eac0006ccaec2
wiheto <william.thompson@ki.se>
2020-03-09 21:04:30 +0100

    calculating analysis 1 per run (since stimsites change per run)

--- esfmri_connectivity/analysis1/calc_fc.py
+++ esfmri_connectivity/analysis1/calc_fc.py
@@ -1,35 +1,32 @@
 import pandas as pd
 from esfmri_connectivity.utils.getfiles import get_timeseries, get_events
 import bct
+import os
+
 com_path = './esfmri_connectivity/communitydetection/data/'
 bids_dir = '/home/william/sherlock/scratch/data/esfmri/'
 save_dir = './esfmri_connectivity/analysis1/data/'
 
 # Grab data for each subject/task and load
-files, savelabels = get_timeseries(group='subtask', task='task-es')
-chosen_resolution = []
+files = get_timeseries(task='task-es', require_stiminfo=True)
 # Have to assume sub-314 runs are the same across all runs
-for fi, filecol in enumerate(files):
-    print(filecol)
-    sub = savelabels[fi].split('_')[0].split('-')[1]
-    ts_eson = []
-    ts_esoff = []
-    for f in filecol:
-        run = f.split('run-')[1].split('_')[0]
-        eson, esoff = get_events(
-            bids_dir, subject=sub, run=run, return_type=None)
-        tstmp = pd.read_csv(f, index_col=[0], sep='\t')
-        ts_eson.append(tstmp[eson.astype(str)].transpose())
-        ts_esoff.append(tstmp[esoff.astype(str)].transpose())
-
-    ts_eson = pd.concat(ts_eson)
-    ts_eson.reset_index(inplace=True, drop=True)
-    ts_esoff = pd.concat(ts_esoff)
-    ts_esoff.reset_index(inplace=True, drop=True)
-
+for _, f in enumerate(files):
+    print(f)
+    savelabels = f.split('/')[-1].split('_space-')[0]
+    sub = savelabels.split('_')[0].split('-')[1]
+    run = f.split('run-')[1].split('_')[0]
+    eson, esoff = get_events(
+    bids_dir, subject=sub, run=run, return_type=None)
+    tstmp = pd.read_csv(f, index_col=[0], sep='\t')
+    ts_eson = tstmp[eson.astype(str)].transpose()
+    ts_esoff = tstmp[esoff.astype(str)].transpose()
+    # ts_eson = pd.concat(ts_eson)
+    # ts_eson.reset_index(inplace=True, drop=True)
+    # ts_esoff = pd.concat(ts_esoff)
+    # ts_esoff.reset_index(inplace=True, drop=True)
     parcel_index = ts_eson.columns
     # Load communities
-    com = pd.read_csv(com_path + savelabels[fi] + '_communities.tsv', sep='\t', index_col=[0])
+    com = pd.read_csv(com_path + 'sub-' + sub + '_task-es' + '_communities.tsv', sep='\t', index_col=[0])
     if any(com.index != parcel_index):
         raise ValueError('indices are different')
     # make connectivity matrices
@@ -48,15 +45,19 @@ for fi, filecol in enumerate(files):
     # Save all the files as tsvs
     df = pd.DataFrame(g_eson, columns=parcel_index, index=parcel_index)
     df.to_csv(save_dir + 'fc/' +
-              savelabels[fi] + '_desc-eson_connectivity.tsv', sep='\t')
+            savelabels + '_desc-eson_connectivity.tsv', sep='\t')
     df = pd.DataFrame(g_esoff, columns=parcel_index, index=parcel_index)
     df.to_csv(save_dir + 'fc/' +
-              savelabels[fi] + '_desc-esoff_connectivity.tsv', sep='\t')
+            savelabels + '_desc-esoff_connectivity.tsv', sep='\t')
     df = pd.DataFrame(data={'participation_coeff': part_eson,
                             'within_module_degree_zscore': z_eson}, index=parcel_index)
     df.to_csv(save_dir + 'cartprofile/' +
-              savelabels[fi] + '_desc-eson_cartprofile.tsv', sep='\t')
+            savelabels + '_desc-eson_cartprofile.tsv', sep='\t')
     df = pd.DataFrame(data={'participation_coeff': part_esoff,
                             'within_module_degree_zscore': z_esoff}, index=parcel_index)
     df.to_csv(save_dir + 'cartprofile/' +
-              savelabels[fi] + '_desc-esoff_cartprofile.tsv', sep='\t')
+            savelabels + '_desc-esoff_cartprofile.tsv', sep='\t')
+
+
+
+
