4282c1be59a067a0dcd9d4cf8c56684b57f8071e
wiheto <william.thompson@ki.se>
2020-06-16 15:26:01 +0200

    label communities (results from docker)

--- esfmri_connectivity/communitydetection/label_communities.py
+++ esfmri_connectivity/communitydetection/label_communities.py
@@ -1,7 +1,7 @@
 import nibabel as nib
 import pandas as pd
 import numpy as np
-
+import os
 
 template = pd.read_csv('esfmri_connectivity/parcellation/tpl-MNI152NLin2009cAsym_res-01_atlas-smorgasbord_dseg.tsv', sep='\t', index_col=[0])
 
@@ -23,16 +23,20 @@ template['comname'] = template_community
 
 com_path = './esfmri_connectivity/communitydetection/data/'
 
-label = {}
+files = os.listdir(com_path)
+files = [f for f in files if 'task-es' in f and 'communities' in f]
 
-sub = '292'
-label['sub'] = {}
-com = pd.read_csv(com_path + 'sub-' + sub + '_task-es' + '_communities.tsv', sep='\t', index_col=[0])
+for f in files: 
 
+    sub = f.split('_')[0].split('-')[1]
+    label = {}
+    com = pd.read_csv(com_path + 'sub-' + sub + '_task-es' + '_communities.tsv', sep='\t', index_col=[0])
 
-for c in com['communities'].unique()
-    c_alt = template.loc[(com['communities'] == c).index]['comname'].values
-    net, counts = np.unique(c_alt, return_counts=True)
-    counts = counts/len(c_alt)
-    label['sub'][c] = net[np.argmax(counts)]
-    
+    for c in com['communities'].unique():
+        c_alt = template.loc[com[com['communities'] == c].index]['comname'].values
+        net, counts = np.unique(c_alt, return_counts=True)
+        counts = counts/len(c_alt)
+        label[c] = net[np.argmax(counts)]
+        
+    df = pd.Series(label)
+    df.to_csv(com_path + sub + '_task-es_labels.tsv', sep='\t')
